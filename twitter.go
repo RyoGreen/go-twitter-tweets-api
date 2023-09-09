bd595613cda124c72cfcd5ec7a8dccc814e00db0
ryo <41415120ryo@gmail.com>
2023-09-09 20:17:36 +0900

    make tweet api function

--- twitter.go
+++ twitter.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -31,15 +32,70 @@ type Response struct {
 	Detail string `json:"detail"`
 }
 
+type ReplaySettingType string
+
+const (
+	MentionedUsers     ReplaySettingType = "mentionedUsers"
+	MentionedFollowing ReplaySettingType = "following"
+)
+
+type Options struct {
+	Text                  string            `json:"text"`
+	DirectMessageDeepLink string            `json:"direct_message_deep_link,omitempty"`
+	ForSuperFollowersOnly bool              `json:"for_super_followers_only,omitempty"`
+	Geo                   *Geo              `json:"geo,omitempty"`
+	Media                 *Media            `json:"media,omitempty"`
+	Poll                  *Poll             `json:"poll,omitempty"`
+	QuoteTweetId          string            `json:"quote_tweet_id,omitempty"`
+	Reply                 *Reply            `json:"reply,omitempty"`
+	ReplySettings         ReplaySettingType `json:"reply_settings,omitempty"`
+}
+
+type Geo struct {
+	PlaceID string `json:"place_id"`
+}
+
+type Media struct {
+	MediaIds      []string `json:"media_ids"`
+	TaggedUserIds []string `json:"tagged_user_ids,omitempty"`
+}
+
+type Poll struct {
+	DurationMinutes uint     `json:"duration_minutes"`
+	Options         []string `json:"options"`
+}
+
+type Reply struct {
+	ExcludeReplyUserIds []string `json:"exclude_reply_user_ids,omitempty"`
+	InReplyToTweetId    string   `json:"in_reply_to_tweet_id"`
+}
+
 var errApiRateLimit = errors.New("error: tweets api rate limit exceeded")
 
-func (c *Client) Tweet(text string) (*Data, error) {
-	options := struct {
-		Text string `json:"text"`
-	}{
-		Text: text,
+const errOptionStr = "error: One or more %v parameters is missing"
+
+func (c *Client) Tweet(o *Options) (*Data, error) {
+	if o.Text == "" {
+		return nil, fmt.Errorf(errOptionStr, "Text")
 	}
-	optionStr, err := json.Marshal(options)
+	if o.Geo != nil && o.Geo.PlaceID == "" {
+		return nil, fmt.Errorf(errOptionStr, "GEO")
+	}
+	if o.Media != nil && o.Media.MediaIds == nil {
+		return nil, fmt.Errorf(errOptionStr, "Media")
+	}
+	if o.Poll != nil && (o.Poll.DurationMinutes == 0 || o.Poll.Options == nil) {
+		return nil, fmt.Errorf(errOptionStr, "Poll")
+	}
+	if o.Reply != nil && o.Reply.InReplyToTweetId == "" {
+		return nil, fmt.Errorf(errOptionStr, "Reply")
+	}
+
+	if (o.Media != nil && (o.Poll != nil || o.Reply != nil)) || (o.Poll != nil && (o.Media != nil || o.Reply != nil)) {
+		return nil, errors.New("error: Media and Quote Tweet ID, Poll are mutually exclusive")
+	}
+
+	optionStr, err := json.Marshal(o)
 	if err != nil {
 		return nil, err
 	}
